b0980311c7aff936b77009b79196da08924d4fbb
Cole McAnelly <cole.mcanelly3@gmail.com>
2023-11-08 23:01:09 -0600

    Added CUDA version of mergesort, and attempted to add CALI markers...

---
+++ cuda-merge/helper.cuh
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <time.h>
+#include <algorithm>
+
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+
+// TYPES
+typedef float seconds_t;
+typedef float milliseconds_t;
+typedef float GBperSecond_t;
+typedef struct ctime {
+  GBperSecond_t bandwidth = 0.0;
+  milliseconds_t sort = 0.0;
+  milliseconds_t copyH2D = 0.0;
+  milliseconds_t copyD2H = 0.0;
+} Timers;
+
+
+// HELPER FUNCTIONS
+
+int random(int) { return (int)rand() % (int)INT_MAX; }
+
+float random(float) { return (float)rand() / (float)RAND_MAX; }
+
+inline void print_elapsed(clock_t start, clock_t stop)
+{
+  double elapsed = ((double)(stop - start)) / CLOCKS_PER_SEC;
+  printf("Elapsed time: %.3fs\n", elapsed);
+}
+
+inline void print_array(int *arr, int length, const char* title = "")
+{
+  printf("%s \n{", title);
+  for (int i = 0; i < length; ++i) printf(" %d,", arr[i]);
+  printf("\b}\n");
+}
+
+inline void print_stats(Timers* ct)
+{
+  printf("Sorting Step Time:\t\t %.6f ms\n", ct->sort);
+  printf("cudaMemcpy Host2Dev Time:\t\t %.6f ms\n", ct->copyH2D);
+  printf("cudaMemcpy Dev2Host Time:\t\t %.6f ms\n", ct->copyD2H);
+  printf("Effective Bandwidth (GB/s):\t\t %.6f GB/s\n", ct->bandwidth);
+}
+
+inline GBperSecond_t bandwidth(seconds_t time, size_t bytes, int num_calls)
+{ // EFFECTIVE BANDWIDTH
+  #define RW_OPS 4
+  return (RW_OPS * num_calls * bytes * 1e-9) / time;
+}
+
+template<typename T>
+inline void fill_array(T* arr, int length, int option)
+{ 
+  CALI_MARK_BEGIN("data_init");
+  switch (option)
+  {
+  case 1:
+    srand(time(NULL));
+    for (int i = 0; i < length; ++i) arr[i] = random(T{});
+    break;
+  case 2:
+    for (int i = 0; i < length; ++i) arr[i] = (T)i;
+    break;
+  case 3:
+    for (int i = 0; i < length; ++i) arr[i] = (T)length-1-i;
+    break;
+  default:
+    break;
+  }
+  CALI_MARK_END("data_init");
+}
+
+template<typename T>
+inline bool check_dealloc(T* __host, T* __dev1, T* __dev2, int len)
+{
+  cudaFree(__dev1);
+  cudaFree(__dev2);
+  // print_array(__host, len, "\nAfter Sort:");
+  CALI_MARK_BEGIN("correctness_check");
+  bool pass = std::is_sorted(&__host[0], &__host[len - 1]);
+  CALI_MARK_END("correctness_check");
+
+  if (pass) printf("\t\033[1m\033[92mSORTED\033[0m\n");
+  else printf("\t\033[1m\033[31mNOT SORTED\033[0m\n");
+  delete[] __host;
+  return pass;
+}
+
+
+// MAJOR STEPS
+
+template<typename T>
+T* alloc_host(int len, int option)
+{
+  T* values = new T[len];
+  fill_array<T>(values, len, option);
+  return values;
+}
+
+template<typename T>
+T* alloc_device(size_t bytes)
+{
+  T* dev_values;
+  cudaMalloc((void **)&dev_values, bytes);
+  return dev_values;
+}
+
+template<typename T>
+milliseconds_t copy(T* dst, T* src, size_t bytes, cudaMemcpyKind direction)
+{
+  // MEM COPY FROM HOST TO DEVICEs
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
+  
+  cudaEventRecord(start);
+  CALI_MARK_BEGIN("comm");
+  CALI_MARK_BEGIN("comm_large");
+  cudaError_t err = cudaMemcpy(dst, src, bytes, direction);
+  CALI_MARK_END("comm_large");
+  CALI_MARK_END("comm");
+  cudaEventRecord(stop);
+
+  cudaEventSynchronize(stop);
+
+  if (err) printf("%s\n", cudaGetErrorString(err));
+
+  milliseconds_t time = 0.0;
+  cudaEventElapsedTime(&time, start, stop);
+  return time;
+}
