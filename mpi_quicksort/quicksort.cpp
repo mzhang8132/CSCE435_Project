71d2e93284e0d51428a66102b8b632bff4c38fc9
saddy00 <76825669+saddy00@users.noreply.github.com>
2023-11-28 15:26:31 -0600

    quicksort mpi changes

--- mpi_quicksort/quicksort.cpp
+++ mpi_quicksort/quicksort.cpp
@@ -10,15 +10,14 @@
 
 /*
 a list of sources:
-- (main one) https://github.com/triasamo1/Quicksort-Parallel-MPI/blob/master/quicksort_mpi.c
-https://www.geeksforgeeks.org/implementation-of-quick-sort-using-mpi-omp-and-posix-thread/#
+- (main one) https://www.geeksforgeeks.org/implementation-of-quick-sort-using-mpi-omp-and-posix-thread/#
 - https://www.codeproject.com/KB/threads/Parallel_Quicksort/Parallel_Quick_sort_without_merge.pdf
 */
 
 int NUM_VALS;
 int OPTION;
 
-const char* options[4] = {"random", "sorted", "reverse_sorted", "1%perturbed"};
+const char* options[4] = {"sorted", "reverse_sorted", "random", "1%perturbed"};
 
 float random_float() {
   return (float)rand()/(float)RAND_MAX;
@@ -61,33 +60,30 @@ void swap(float *x, float *y) {
 }
 
 
-int partition(float *values, int left, int right) {
-    int i = left, j = right;
-    float pivot = values[(left + right) / 2];
+void quicksort(float* values, int left, int right) {
+    if (right <= 1) {
+        return;
+    }
 
-    while (i <= j) {
-        while (values[i] < pivot)
-            i++;
-        while (values[j] > pivot)
-            j--;
-        if (i <= j) {
-            swap(&values[i], &values[j]);
-            i++;
-            j--;
+    float pivot = values[left + right / 2];
+    swap(&values[left], &values[left + right / 2]);
+
+    int index = left;
+
+    for (int i = left + 1; i < left + right; i++) {
+        if (values[i] < pivot) {
+            index++;
+            swap(&values[i], &values[index]);
         }
     }
-    return j;
+
+    swap(&values[left], &values[index]);
+
+    quicksort(values, left, index - left);
+    quicksort(values, index + 1, left + right - index - 1);
 }
 
-void quicksort(float *values, int left, int right) {
-    if (left < right) {
-        int pivot_index = partition(values, left, right);
-        quicksort(values, left, pivot_index);
-        quicksort(values, pivot_index + 1, right);
-    }
-}
-
-float* merge(float* arr1, int n1, float* arr2, int n2) {
+float* merge_sorted(float* arr1, int n1, float* arr2, int n2) {
     float* result = (float*)malloc((n1 + n2) * sizeof(float));
     int i = 0, j = 0, k = 0;
     while (i < n1 && j < n2)
@@ -99,12 +95,6 @@ float* merge(float* arr1, int n1, float* arr2, int n2) {
     return result;
 }
 
-void assign(float* values, float* arr, int offset, int rank) {
-    for (int i = 0; i < offset; ++i) {
-        values[i+offset*rank] = arr[i];
-    }
-}
-
 int check(float* values, int length) {
     for (int i = 1; i < length; ++i) {
         if (values[i] < values[i -1]) {
@@ -114,37 +104,50 @@ int check(float* values, int length) {
     return 1;
 }
 
-
 int main(int argc, char *argv[]) {
     CALI_CXX_MARK_FUNCTION;
 
     int numtasks, taskid, chunk_size, local_chunk_size;
+    float *data = NULL, *chunk = NULL, *sorted = NULL;
 
     NUM_VALS = atoi(argv[1]);
     OPTION = atoi(argv[2]);
 
     MPI_Status status;
 
-    MPI_Init(&argc, &argv);
+    if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
+        fprintf(stderr, "MPI Initialization failed.\n");
+        return -1;
+    }
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
     cali::ConfigManager mgr;
     mgr.start();
 
+    data = (float*)malloc(NUM_VALS * sizeof(float));
+    CALI_MARK_BEGIN("data_init");
+    array_fill(data, NUM_VALS, OPTION, taskid, numtasks);
+    CALI_MARK_END("data_init");
+
+    CALI_MARK_BEGIN("comm");
+    CALI_MARK_BEGIN("comm_large");
+    CALI_MARK_BEGIN("MPI_Barrier");
+    MPI_Barrier(MPI_COMM_WORLD);
+    CALI_MARK_END("MPI_Barrier");
+    CALI_MARK_END("comm_large");
+    CALI_MARK_END("comm");
+
+    CALI_MARK_BEGIN("comm");
+    CALI_MARK_BEGIN("comm_large");
+    CALI_MARK_BEGIN("MPI_Bcast");
+    MPI_Bcast(&NUM_VALS, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    CALI_MARK_END("MPI_Bcast");
+    CALI_MARK_END("comm_large");
+    CALI_MARK_END("comm");
+
     chunk_size = (NUM_VALS % numtasks == 0) ? (NUM_VALS / numtasks) : (NUM_VALS / (numtasks - 1));
-
-    float *chunk = (float*)malloc(chunk_size * sizeof(float));
-
-    float *data = NULL;
-    if (taskid == 0) {
-        data = (float*)malloc(NUM_VALS * sizeof(float));
-        CALI_MARK_BEGIN("data_init");
-        array_fill(data, NUM_VALS, OPTION, taskid, numtasks);
-        CALI_MARK_END("data_init");
-    }
-    
-    double start_timer = MPI_Wtime();
+    chunk = (float*)malloc(chunk_size * sizeof(float));
 
     CALI_MARK_BEGIN("comm");
     CALI_MARK_BEGIN("comm_large");
@@ -154,10 +157,9 @@ int main(int argc, char *argv[]) {
     CALI_MARK_END("comm_large");
     CALI_MARK_END("comm");
 
-    if (taskid == 0) {
-        free(data);
-    }
+    free(data);
     
+    // float *data = NULL;
     local_chunk_size = (NUM_VALS >= chunk_size * (taskid + 1)) ? chunk_size : (NUM_VALS - chunk_size * taskid);
     
     CALI_MARK_BEGIN("comp");
@@ -166,10 +168,46 @@ int main(int argc, char *argv[]) {
     CALI_MARK_END("comp_large");
     CALI_MARK_END("comp");
     
-    float *sorted = NULL;
-    if (taskid == 0) {
-        sorted = (float*)malloc(NUM_VALS * sizeof(float));
+    for (int step = 1; step < numtasks; step *= 2) {
+        if (taskid % (2 * step) != 0) {
+            CALI_MARK_BEGIN("comm");
+            CALI_MARK_BEGIN("comm_large");
+            CALI_MARK_BEGIN("MPI_Send");
+            MPI_Send(chunk, local_chunk_size, MPI_FLOAT, taskid - step, 0, MPI_COMM_WORLD);
+            CALI_MARK_END("MPI_Send");
+            CALI_MARK_END("comm_large");
+            CALI_MARK_END("comm");
+            break;
+        }
+
+        if (taskid + step < numtasks) {
+            int received_chunk_size = (NUM_VALS >= chunk_size * (taskid + 2 * step)) ? (chunk_size * step) : (NUM_VALS - chunk_size * (taskid + step));
+
+            float* received_chunk = (float*)malloc(received_chunk_size * sizeof(float));
+
+            CALI_MARK_BEGIN("comm");
+            CALI_MARK_BEGIN("comm_large");
+            CALI_MARK_BEGIN("MPI_Recv");
+            MPI_Recv(received_chunk, received_chunk_size, MPI_FLOAT, taskid + step, 0, MPI_COMM_WORLD, &status);
+            CALI_MARK_END("MPI_Recv");
+            CALI_MARK_END("comm_large");
+            CALI_MARK_END("comm");
+
+            CALI_MARK_BEGIN("comp");
+            CALI_MARK_BEGIN("comp_large");
+            sorted = merge_sorted(chunk, local_chunk_size, received_chunk, received_chunk_size);
+            CALI_MARK_END("comp_large");
+            CALI_MARK_END("comp");
+
+            free(chunk);
+            free(received_chunk);
+            chunk = sorted;
+            local_chunk_size += received_chunk_size;
+        }
     }
+
+    sorted = (float*)malloc(NUM_VALS * sizeof(float));  
+
     CALI_MARK_BEGIN("comm");
     CALI_MARK_BEGIN("comm_large");
     CALI_MARK_BEGIN("MPI_Gather");
@@ -177,22 +215,11 @@ int main(int argc, char *argv[]) {
     CALI_MARK_END("MPI_Gather");
     CALI_MARK_END("comm_large");
     CALI_MARK_END("comm");
-    free(chunk);
 
     if (taskid == 0) {
         CALI_MARK_BEGIN("correctness_check");
         int correctness = check(sorted, NUM_VALS);
         CALI_MARK_END("correctness_check");
-        
-        /*
-        for (int i = 0; i < NUM_VALS; i++){
-        printf("%f ", global_list[i]);
-        }
-        printf("\n");
-        */
-        
-        double finish_timer = MPI_Wtime();
-        // printf("Total time: %2.0f seconds\n", finish_timer - start_timer);
 
         adiak::init(NULL);
         adiak::launchdate();    // launch date of the job
@@ -210,11 +237,13 @@ int main(int argc, char *argv[]) {
         adiak::value("implementation_source", "Online, Handwritten, and AI"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
         adiak::value("correctness", correctness); // Whether the dataset has been sorted (0, 1)
 
-        free(sorted);
+        // free(sorted);
     }
     
     mgr.stop();
     mgr.flush();
 
     MPI_Finalize();
+
+    free(sorted);
 }
